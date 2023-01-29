9f405a10e4f9d16de6a741e2f00c565433238f1d
Cristian Eduardo Romero Valdes <cristianromerovs@gmail.com>
2023-01-28 22:48:35 -0300

    Server side funcionando

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cors = require('cors')
 const app = express()
+const port = 8000;
+const whiteList = [`http://localhost:${port}`]
 
-//Rutas
+app.use(cors());
+
+// Llamado rutas
 const routes = require('./src/routes/index.routes');
 
 app.use(routes);
 
-app.listen(8000, () => {console.log("Server running at port: 8000")});
+app.listen(8000, () => {console.log(`Server running at port: ${port}`)});
